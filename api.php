07c34ef4303eb79713d41349b4ab86732f214a0f
omarkhaled321 <162824746+omarkhaled321@users.noreply.github.com>
2024-08-21 13:42:51 +0300

    Delete api.php

--- api.php
+++
@@ -1,8 +0,0 @@
-<?php
-use App\Http\Controllers\Api\UserController;
-use App\Http\Controllers\Api\FlightController;
-
-
-Route::get('/users', [UserController::class, 'index']);
-Route::get('/flights', [FlightController::class, 'index']);
-Route::get('/flights/{flight}/passengers', [FlightController::class, 'passengers']);
